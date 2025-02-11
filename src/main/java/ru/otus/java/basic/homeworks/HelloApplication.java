e84f85b29a46bc2d26923cefed5c6155eccc6926
gammu3 <gammu3@gmail.com>
2025-02-11 19:27:27 +0400

    init comit

---
+++ src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks;
+
+public class HelloApplication {
+    public static void main(String[] args) {
+        System.out.println("Проект ДЗ по Java basic");
+    }
+}
