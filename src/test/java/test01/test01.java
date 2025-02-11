27f5e1f005db824d5c996d7ca69a7000f321b0b5
gammu3 <gammu3@gmail.com>
2025-02-11 19:39:00 +0400

    init comit 4 test01 fix

--- src/test/java/test01/test01.java
+++ src/test/java/test01/test01.java
@@ -3,5 +3,6 @@ package test01;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Тестовый пакет/класс");
+        System.out.println("Hello word!");
     }
 }
